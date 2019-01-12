7ff04842d76c55323e0fe48160c931da1ed3ed86
Song <yousonghou@yahoo.com>
2019-01-12 00:41:43 +0000

    add getFood, editFood, removeFood fetch methods

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const registerButton = document.getElementById('register');
 const loginButton = document.getElementById('login');
 
 const getFoodsButton = document.getElementById('getFoods');
+const getFoodButton = document.getElementById('getFood');
 const postFoodButton = document.getElementById('postFood');
+const editFoodButton = document.getElementById('editFood');
+const removeFoodButton = document.getElementById('removeFood');
 
 const token = localStorage.getItem('token');
 
@@ -50,6 +53,13 @@ getFoodsButton.addEventListener('click', () => {
     .catch(err => console.log(err));
 });
 
+getFoodButton.addEventListener('click', () => {
+  fetch('https://cheapnycserver.herokuapp.com/foods/5c391f53a30dd600041e81ff')
+    .then(res => res.json())
+    .then(resData => console.log(resData))
+    .catch(err => console.log(err));
+});
+
 postFoodButton.addEventListener('click', () => {
   fetch('https://cheapnycserver.herokuapp.com/foods', {
     method: 'POST',
@@ -59,6 +69,38 @@ postFoodButton.addEventListener('click', () => {
       location: '123 Apple St',
       description: "This is a good apple"
     }),
+    headers: {
+      'Authorization': token,
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(resData => console.log(resData))
+    .catch(err => console.log(err));
+});
+
+editFoodButton.addEventListener('click', () => {
+  fetch('https://cheapnycserver.herokuapp.com/foods/5c391f53a30dd600041e81ff', {
+    method: 'PUT',
+    body: JSON.stringify({
+      name: 'Watermelons',
+      price: '1.99',
+      location: '123 Fruit St',
+      description: "This is fresh from the farm."
+    }),
+    headers: {
+      'Authorization': token,
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(resData => console.log(resData))
+    .catch(err => console.log(err));
+});
+
+removeFoodButton.addEventListener('click', () => {
+  fetch('https://cheapnycserver.herokuapp.com/foods/5c39367c697b8b0004747ca4', {
+    method: 'DELETE',
     headers: {
       'Authorization': token,
       'Content-Type': 'application/json'
